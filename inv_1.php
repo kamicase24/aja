9cc9b0d49a4b6579df74138b1b0059e6845541f8
Jesus Rojas <kamicase24@hotmail.com>
2016-09-23 00:29:15 -0400

    finalizado el crud de productos con ajax

--- inv_1.php
+++ inv_1.php
@@ -13,84 +13,6 @@ include('lib.php')
 		<link rel="stylesheet" type="text/css" href="src/css/custom/inv_custom.css">
 		<script type="text/javascript" src="src/js/jquery.min.js"></script>
 		<script type="text/javascript" src="src/js/bootstrap.min.js"></script>
-<!-- 		<script>
-			function eliminar(puntero){
-				modal_eliminar.style.display = "block";
-				h_input = document.createElement("input");
-				h_input.setAttribute("type","text");
-				h_input.setAttribute("value",puntero);
-				h_input.setAttribute("hidden",true);
-				h_input.setAttribute("id","puntero");
-				document.getElementById("eliminar-modal-body").appendChild(h_input);
-			}
-			function eliminar_aceptar(){
-					var puntero = $("input#puntero").val();
-					var parametros = {"action":"eliminar","puntero":puntero};
-					$.ajax({
-						type: "POST",
-						url: "1.php",
-						data: parametros,
-						success: function(a) {
-							window.location="inv_1.php";
-						}
-					});
-				}
-			function modificar(puntero){
-				var parametros = {"action":"ajax","puntero":puntero};
-				$.ajax({
-					type: "POST",
-					url: "1.php",
-					data: parametros,
-					success: function(a) {
-						$('#modificar-modal-body').html(a);
-					}
-				});
-				modal.style.display = "block";
-			}
-			function modificar_submit(){
-
-				var id_pro = $("input#id_pro").val();
-				var nom_pro = $("input#nom_pro").val();
-				var det_pro = $("input#det_pro").val();
-				var id_med = $("select#med option:selected").val();
-				var parametros = {"action":"modificar",
-								  "id_pro":id_pro,
-								  "nom_pro":nom_pro,
-								  "det_pro":det_pro,
-								  "id_med" :id_med};
-				$.ajax({
-					type: "POST",
-					url: "1.php",
-					data: parametros,
-					success: function(a) {
-						window.location="inv_1.php"
-					}
-				});
-			}
-			function agregar(){
-				modal_agregar.style.display = "block";
-			}
-			function cerrar_agregar(){
-				modal_agregar.style.display = "none";
-			}
-			function cerrar_modificar(){
-				modal.style.display = "none";
-			}
-			function cerrar_eliminar(){
-				modal_eliminar.style.display = "none";
-			}
-			window.onclick = function(event) {
-				if (event.target == modal) {
-					modal.style.display = "none";
-				}
-				if (event.target == modal_agregar) {
-					modal_agregar.style.display = "none";
-				}
-				if (event.target == modal_eliminar) {
-					modal_eliminar.style.display = "none";
-				}
-			}
-		</script> -->
 		<script type="text/javascript" src="src/js/odontoiut.js"></script>
 	</head>
 <body>
@@ -170,14 +92,15 @@ include('lib.php')
 				<h2>Nuevo producto</h2>
 			</div>
 			<div class="modal-body">
-					<input type="text" name="nom_pro" placeholder="Producto" class="form-control">
-					<input type="text" name="det_pro" placeholder="Detalle" class="form-control">
-					<select name="med" class="form-control">
+					<input type="text" name="nom_pro" id="nom_pro" placeholder="Producto" class="form-control">
+					<input type="text" name="det_pro" id="det_pro" placeholder="Detalle" class="form-control">
+					<select name="med" id="med" class="form-control">
 						<option value="1">Ml.</option>
 					</select>
 			</div>
 			<div class="modal-footer">
 				<input type="submit" name="bt_pro" class="btn btn-primary form-control btn-sm" value="Nuevo">
+				<button type="button" class="btn btn-primary btn-sm" onclick="agregar_submit()">
 			</div>
 			</form>
 		</div>
@@ -187,6 +110,8 @@ include('lib.php')
 		<div class="col-md-6">
 			<h3 id="id_pro">Lista de Productos</h3>
 		</div>
+
+		<!-- boton agregar -->
 		<div class="col-md-6">
 			<h3 class="text-right">
 			<button
@@ -199,8 +124,8 @@ include('lib.php')
 			</h3>
 		</div>
 
-		<div class="col-md-12">
-			<table id="tabla1" class="table table-bordered">
+		<div class="col-md-12 prod_list">
+			<table id="tabla1" class="table table-bordered prod_list">
 				<thead>
 					<th>#</th>
 					<th>Producto</th>
@@ -222,19 +147,10 @@ include('lib.php')
 							<td><?php print $var[2]; ?></td>
 							<td><?php print $var[3]; ?></td>
 							<td>
-							<button
-								type="button"
-								class="btn btn-primary"
-								onclick="modificar('<?php print $var[0]; ?>')"
-								id="btn-modal-modificar"
-							>
-								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Modificar
+							<button type="button" class="btn btn-primary" onclick="modificar('<?php print $var[0]; ?>')" id="btn-modal-modificar">
+							<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Modificar
 							</button>
-							<button
-								type="button"
-								class="btn btn-danger btn-sm"
-								onclick="eliminar('<?php print $var[0]; ?>')"
-							>
+							<button type="button" class="btn btn-danger btn-sm"	onclick="eliminar('<?php print $var[0]; ?>')">
 							<span class="glyphicon glyphicon-trash"></span>
 							</button>
 							</td>
@@ -244,28 +160,10 @@ include('lib.php')
 				?>
 			</table>
 		</div>
-		<div class="row">
-			<?php
-				// Agrega
-				if (isset($_POST['bt_pro'])) {
-
-					$nom_pro = $_POST['nom_pro']; $det_pro = $_POST['det_pro'];
-					$med 	 = $_POST['med'];
-
-					$db = new Database_pro; $con = $db->conecta();
-
-					$sql = "INSERT INTO producto(nom_pro,det_pro,med) VALUES ('$nom_pro', '$det_pro',$med)";
-					$query = pg_query($con, $sql);
-
-					$odontolib = new Odontoiut2;
-					print $odontolib->redireccion('inv_1.php','Guardado Exitosamente');
-				}
-			?>
 			<script>
 				var modal_agregar = document.getElementById("agregar-modal");
 				var modal = document.getElementById("modificar-modal");
 				var modal_eliminar = document.getElementById("elimitar-modal");
 			</script>
-		</div>
 	</div>
 </body>
